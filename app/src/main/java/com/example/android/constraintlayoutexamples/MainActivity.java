4081b8412f7e21115c1b41d4395f1cc75889bc45
sherchen <ncuboy_045wsq@qq.com>
2017-07-25 16:32:27 +0800

    f

--- app/src/main/java/com/example/android/constraintlayoutexamples/MainActivity.java
+++ app/src/main/java/com/example/android/constraintlayoutexamples/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_test);
     }
 
     public void show(View v) {
