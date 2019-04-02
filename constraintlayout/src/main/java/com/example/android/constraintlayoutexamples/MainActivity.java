e52546a1baeb290434e0a8e71c0bf3b5f10f13ae
Takeshi Hagikura <shiketa1999@gmail.com>
2019-04-02 10:59:14 +0900

    Migrate ConstraintLayout examples to androidx. (#68)
    Some cleanups.
    - Extract the depdencies versions to the top-level build.gradle
    - Removed unused dependencies

--- constraintlayout/src/main/java/com/example/android/constraintlayoutexamples/MainActivity.java
+++ constraintlayout/src/main/java/com/example/android/constraintlayoutexamples/MainActivity.java
@@ -19,8 +19,9 @@ package com.example.android.constraintlayoutexamples;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private String mTag = "activity_main";
